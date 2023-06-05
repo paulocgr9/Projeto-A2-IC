7a7155f7574f1269745cf57fce9fd22bf8027e39
paulocgr9 <126474247+paulocgr9@users.noreply.github.com>
2023-06-05 15:37:37 -0300

    Mais correções na questão 5

--- Descrição/a2.py
+++ Descrição/a2.py
@@ -12,7 +12,7 @@ def questao_2(caminho):
     casos_por_municipio = df["ID_MUNICIP"].value_counts()
     return casos_por_municipio
 
-def questao_3():
+def questao_3(caminho):
     df = pd.read_csv(caminho)
     dicionario =  df["CS_SEXO"].value_counts().to_dict()
     if " " in dicionario.keys():
@@ -27,10 +27,10 @@ def questao_4():
 def questao_5(caminho):
     df = pd.read_csv(caminho)
     estados_com_sigla = df["SG_UF_NOT"].replace(
-        {"12":"AC", "27":"AL", "16":"AP", "13":"AM", "29": "BA", "23":"CE", "53":"DF", 
-        "32":"ES", "52":"GO", "21":"MA", "51":"MT", "50":"MS", "31":"MG", "15":"PA",
-        "25":"PB", "41":"PR", "26":"PE", "22":"PI", "24":"RN", "43":"RS", "33":"RJ", 
-        "11":"RO", "14":"RR", "42":"SC", "35":"SP", "28":"SE", "17":"TO"}
+        {12:"AC", 27:"AL", 16:"AP", 13:"AM", 29:"BA", 23:"CE", 53:"DF", 
+        32:"ES", 52:"GO", 21:"MA", 51:"MT", 50:"MS", 31:"MG", 15:"PA",
+        25:"PB", 41:"PR", 26:"PE", 22:"PI", 24:"RN", 43:"RS", 33:"RJ", 
+        11:"RO", 14:"RR", 42:"SC", 35:"SP", 28:"SE", 17:"TO"}
     ).to_frame()
     contagem_por_estado = estados_com_sigla.value_counts().to_dict()
     return contagem_por_estado
