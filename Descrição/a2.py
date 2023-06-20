e1a675fd1b035f3d9acfe0c5e2ee18cc3cd2c5f8
pedrotokar <pedrotokar2004@gmail.com>
2023-06-20 20:23:43 -0300

    Adicionei um argumento padrão para as funções com o dataset de chagas.

--- Descrição/a2.py
+++ Descrição/a2.py
@@ -11,16 +11,16 @@ dicionario_estados = {
     35:"SP", 28:"SE", 17:"TO"
 }
 
-def questao_1(caminho):
+def questao_1(caminho = "chagas2018.csv"):
     df = pd.read_csv(caminho)
     return df.shape[0]
 
-def questao_2(caminho):
+def questao_2(caminho = "chagas2018.csv"):
     df = pd.read_csv(caminho)
     casos_por_municipio = df["ID_MUNICIP"].value_counts()
     return casos_por_municipio
 
-def questao_3(caminho):
+def questao_3(caminho = "chagas2018.csv"):
     df = pd.read_csv(caminho)
     dicionario =  df["CS_SEXO"].value_counts().to_dict()
     if " " in dicionario.keys():
@@ -38,26 +38,26 @@ def questao_3(caminho):
     else:
         return "F", dicionario
 
-def questao_4(caminho):
+def questao_4(caminho = "chagas2018.csv"):
     df = pd.read_csv(caminho)
     df["IDADE_DECODIFICADA"] = decodifica_idade_SINAN(df.NU_IDADE_N, "Y")
     media = df["IDADE_DECODIFICADA"].mean()
     return media
 
-def questao_5(caminho):
+def questao_5(caminho = "chagas2018.csv"):
     df = pd.read_csv(caminho)
     estados_com_sigla = df["SG_UF_NOT"].replace(dicionario_estados)
     contagem_por_estado = estados_com_sigla.value_counts().to_dict()
     return contagem_por_estado
 
-def questao_6(caminho):
+def questao_6(caminho = "chagas2018.csv"):
     df = pd.read_csv(caminho)
     df["SG_UF_NOT"] = df["SG_UF_NOT"].replace(dicionario_estados)
     somente_homens = df[df["CS_SEXO"] == "M"]
     quant_por_estado = somente_homens["SG_UF_NOT"].value_counts().to_dict()
     return quant_por_estado
 
-def questao_7(caminho):
+def questao_7(caminho = "chagas2018.csv"):
     df = pd.read_csv(caminho)
     cidades_por_uf = {"AC":22, "AL":102, "AP":16, "AM":62, "BA":417, "CE":184,
                       "DF":1, "ES":78, "GO":246, "MA":217, "MT":141, "MS":79,
@@ -75,7 +75,7 @@ def questao_7(caminho):
             proporcao_por_uf[estado] = 0.0
     return proporcao_por_uf
 
-def questao_8(caminho):
+def questao_8(caminho = "chagas2018.csv"):
     df = pd.read_csv(caminho)
     df = df.astype({"DT_NOTIFIC": str, "DT_SIN_PRI": str})
     df["DT_NOTIFICACAO"] = pd.to_datetime(df["DT_NOTIFIC"])
@@ -83,7 +83,7 @@ def questao_8(caminho):
     df["ATRASO_NOT"] = (df["DT_NOTIFICACAO"] - df["DT_SINTOMAS"]).dt.days
     return df[["DT_NOTIFICACAO", "DT_SINTOMAS", "ATRASO_NOT"]]
 
-def questao_9(caminho):
+def questao_9(caminho = "chagas2018.csv"):
     df = pd.read_csv(caminho)
     medias_e_desvios = dict()
     df["SG_UF_NOT"] = df["SG_UF_NOT"].replace(dicionario_estados)
@@ -100,7 +100,7 @@ def questao_9(caminho):
             medias_e_desvios[estado] = (0, 0)
     return medias_e_desvios
 
-def questao_10(caminho):
+def questao_10(caminho = "chagas2018.csv"):
     df = pd.read_csv(caminho)
     df = df.astype({"DT_NOTIFIC": str, "DT_SIN_PRI": str})
     df["DT_NOTIFICACAO"] = pd.to_datetime(df["DT_NOTIFIC"])
@@ -114,7 +114,7 @@ def questao_10(caminho):
         x="NUM_CASOS",
         y="ATRASO_NOT",
         title="Quantidade de Casos por Média de Atraso de Notificação por Município",
-        xlabel="Númmero de Casos",
+        xlabel="Número de Casos",
         ylabel = "Média de Atraso de Notificação",
         color="red")
     return media_atraso_mun
--- Descrição/auxiliar.py
+++
@@ -1,3 +0,0 @@
-import a2
-import pandas
-print(a2.questao_1("paralisiainfantil.csv"))
